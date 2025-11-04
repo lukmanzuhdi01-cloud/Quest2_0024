c97fc969d889f640e982703d35e95182c777592f
lukmanzuhdi01-cloud <lukmanzuhdi01@gmail.com>
2025-11-04 18:07:36 +0700

    memanggil variable

--- project1.java
+++ project1.java
@@ -11,6 +11,19 @@ public class project1 {
       System.out.print("Program seleksi asistenD");
       System.out.print("masukan nilai mahasiswa: ");
 
+      System.out.print("Masukan nilai Sdata: ");
+      Sdata = ns.nextDouble();
+      System.out.print("Masukan nilai PBobjek: ");
+      PBobjek = ns.nextDouble();
+      System.out.print("Masukan nilai Bdata: ");
+      Bdata = ns.nextDouble();
+      System.out.print("Masukan nilai algoritmaP: ");
+      algoritmaP = ns.nextDouble();
+      System.out.print("Masukan nilai RPlunak: ");
+      RPlunak = ns.nextDouble();
+      double totalNilai = (Sdata + PBobjek + Bdata + algoritmaP + RPlunak) / 5;
+      System.out.println("Total nilai: " + totalNilai);
+
       
 
 
