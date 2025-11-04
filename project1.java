6362e523906155ce7f75716e1b54191735c977bc
lukmanzuhdi01-cloud <lukmanzuhdi01@gmail.com>
2025-11-04 17:57:41 +0700

    memasukkan variabel

--- project1.java
+++ project1.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class project1 {
     public static void main(String[] args) {
+      Scanner ns = new Scanner(System.in);
+      double Sdata;
+      double PBobjek;
+      double Bdata;
+      double algoritmaP;
+      double RPlunak;
+
       
       
     }
